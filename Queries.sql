7523a45df6334aff9f2027f4dfc8b23ec80f255e
Adeel <adeel.maliks20@gmail.com>
2018-03-30 16:21:40 +0300

    imdb

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,4 @@
 SELECT title, year, CAST(overseas_percent AS FLOAT) 'Money Earned % Overseas' 
 FROM top_movies 
 WHERE overseas_percent > 60.0 AND year= '2010';
+/* the list is exactly like this http://www.imdb.com/list/ls055899579/ */
